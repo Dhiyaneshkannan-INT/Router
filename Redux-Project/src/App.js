78b1e1d8a8d26b9524993f35cc47b5c7ba9996f1
Dhiyanesh Kannan <dhiyaneshkannan.r@indusnet.co.in>
2022-10-14 12:06:00 +0530

    Router added

--- Redux-Project/src/App.js
+++ Redux-Project/src/App.js
@@ -2,14 +2,29 @@ import "./App.css";
 import Contact from "./Pages/Contact";
 import Home from "./Pages/Home";
 import User from "./Pages/User";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Contact />
-      <User />
-    </div>
+    <Router>
+      <div className="App">
+        <ul className="App-header">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route exact path="/" component={<Home />}></Route>
+          <Route exact path="/user" component={<User />}></Route>
+          <Route exact path="/contact" component={<Contact />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
