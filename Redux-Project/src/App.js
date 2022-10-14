293436219d578c57bcac8613beedb1595861da76
Dhiyanesh Kannan <dhiyaneshkannan.r@indusnet.co.in>
2022-10-14 11:57:39 +0530

    Second Commit

--- Redux-Project/src/App.js
+++ Redux-Project/src/App.js
@@ -1,10 +1,14 @@
- 
-import './App.css';
+import "./App.css";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import User from "./Pages/User";
 
 function App() {
   return (
     <div className="App">
-     
+      <Home />
+      <Contact />
+      <User />
     </div>
   );
 }
---
+++ Redux-Project/src/Pages/User.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const User = () => {
+  return (
+    <div>
+      <h4>User</h4>
+    </div>
+  );
+};
+
+export default User;
